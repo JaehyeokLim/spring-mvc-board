9046576c76b8d41233d385686a889a004d1eabbb
JaehyeokLim <217686834+JaehyeokLim@users.noreply.github.com>
2025-08-19 16:24:46 +0900

    ✨ feat: Spring MVC Board 프로젝트 초기 설정

---
+++ src/main/java/com/jaehyeoklim/spring/mvc/board/SpringMvcBoardApplication.java
@@ -0,0 +1,13 @@
+package com.jaehyeoklim.spring.mvc.board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcBoardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jaehyeoklim/spring/mvc/board/SpringMvcBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jaehyeoklim.spring.mvc.board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
