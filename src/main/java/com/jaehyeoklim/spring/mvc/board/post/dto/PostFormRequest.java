4a659a25a5346932df8138625da07046e5e920f7
JaehyeokLim <217686834+JaehyeokLim@users.noreply.github.com>
2025-08-23 01:54:39 +0900

    ♻️ refactor: Bean Validator 추가 및 매핑 에러 메시지 추가

--- src/main/java/com/jaehyeoklim/spring/mvc/board/post/dto/PostFormRequest.java
+++ src/main/java/com/jaehyeoklim/spring/mvc/board/post/dto/PostFormRequest.java
@@ -1,12 +1,15 @@
 package com.jaehyeoklim.spring.mvc.board.post.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 public record PostFormRequest(
 
-        @NotBlank
+        @NotBlank(message = "{NotBlank.post.title}")
+        @Size(min = 2, max = 40)
         String title,
 
-        @NotBlank
+        @NotBlank(message = "내용은 필수입니다.")
+        @Size(min = 2, max = 1000)
         String content
 ) {}
--- src/main/java/com/jaehyeoklim/spring/mvc/board/post/dto/PostUpdateRequest.java
+++ src/main/java/com/jaehyeoklim/spring/mvc/board/post/dto/PostUpdateRequest.java
@@ -1,9 +1,15 @@
 package com.jaehyeoklim.spring.mvc.board.post.dto;
 
-import java.util.UUID;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 public record PostUpdateRequest(
 
+        @NotBlank(message = "{NotBlank.post.title}")
+        @Size(min = 2, max = 40)
         String title,
+
+        @NotBlank(message = "내용은 필수입니다.")
+        @Size(min = 2, max = 1000)
         String content
 ) {}
