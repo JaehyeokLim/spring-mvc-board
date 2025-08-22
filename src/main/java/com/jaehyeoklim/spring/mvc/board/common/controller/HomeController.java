0e4a37ec88529657289a7e07b02ef8af169a0cce
JaehyeokLim <217686834+JaehyeokLim@users.noreply.github.com>
2025-08-22 16:32:56 +0900

    ✨ feat: 홈 화면에 게시글 목록 출력 기능 추가 - HomeController에서 전체 게시글 조회 후 posts 모델에 추가 - index.html에 게시글 목록 테이블 및 스타일 적용

--- src/main/java/com/jaehyeoklim/spring/mvc/board/common/controller/HomeController.java
+++ src/main/java/com/jaehyeoklim/spring/mvc/board/common/controller/HomeController.java
@@ -1,17 +1,28 @@
 package com.jaehyeoklim.spring.mvc.board.common.controller;
 
 import com.jaehyeoklim.spring.mvc.board.common.advice.UseLoginUser;
+import com.jaehyeoklim.spring.mvc.board.post.dto.PostDto;
+import com.jaehyeoklim.spring.mvc.board.post.service.PostService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Slf4j
 @Controller
 @UseLoginUser
+@RequiredArgsConstructor
 public class HomeController {
 
-    @GetMapping()
-    public String index() {
+    private final PostService postService;
+
+    @GetMapping
+    public String index(Model model) {
+        List<PostDto> allPosts = postService.findAllPosts();
+        model.addAttribute("posts", allPosts);
         return "index";
     }
 }
